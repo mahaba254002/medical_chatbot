bc840b9decbade919d49517009a40c482f3ebedf
Bakari Salim Mahaba <mahaba955@gmail.com>
2025-06-09 20:23:49 +0800

    edited templates

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,7 @@
 system_prompt = (
     "You are an assistant for question-answering tasks. "
     "Use **only** the following pieces of retrieved context to answer "
-    "the question. If the context does not provide enough information, "
+    "the question and summarize it and act like a doctor. If the context does not provide enough information, "
     "or if the question is unrelated to the context, say 'I don't know.' "
     "Do not use any external knowledge or make up answers."
     "\n\n"
